d5e659382cdc0932d0a869e9b533026d98d6dbe6
brunofiegenbaum <bruno.fiegenbaum@universo.univates.br>
2026-09-24 19:55:52 -0300

    Usa CSS enxuto no formulário PHP

--- formulario.php
+++ formulario.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contato - Bruno Automobilísticos</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-php.css">
 </head>
 <body>
 <header>
